1d6cf0dadd88f6da6f3b68a376da6cbeee10edb7
hbujakow <h.bujakow@gmail.com>
2023-04-28 09:35:50 +0200

    update existing classes, reformat

--- features/transform.py
+++ features/transform.py
@@ -16,31 +16,36 @@ class Tree:
         if n <= 1:
             return 0
         else:
-            depths = [self.depth(comment_tree[comment_id])
-                      for comment_id in comment_tree]
-            return sum(depths) / (n * (n-1)) + 1
+            depths = [
+                self.depth(comment_tree[comment_id]) for comment_id in comment_tree
+            ]
+            return sum(depths) / (n * (n - 1)) + 1
 
     def depth(self, comment_tree=None, depth=0):
-        comment_tree = comment_tree if isinstance(
-            comment_tree, dict) else self.structure
+        comment_tree = (
+            comment_tree if isinstance(comment_tree, dict) else self.structure
+        )
         if not comment_tree:
             return depth
         depths = []
         for child_comments in comment_tree.values():
-            depths.append(self.depth(child_comments, depth=depth+1))
+            depths.append(self.depth(child_comments, depth=depth + 1))
         return max(depths) + 1
 
     def _create_structure(self, parent_id=None):
         parent_id = self.post.id.values[0] if parent_id is None else parent_id
         tree = {}
-        for i, row in self.comments_df[self.comments_df['parent_id'] == parent_id].iterrows():
-            comment_id = row['id']
+        for i, row in self.comments_df[
+            self.comments_df["parent_id"] == parent_id
+        ].iterrows():
+            comment_id = row["id"]
             tree[comment_id] = self._create_structure(comment_id)
         return tree
 
     def print_comment_tree(self, comment_tree=None, indent=0):
-        comment_tree = comment_tree if isinstance(
-            comment_tree, dict) else self.structure
+        comment_tree = (
+            comment_tree if isinstance(comment_tree, dict) else self.structure
+        )
         for comment_id, child_comments in comment_tree.items():
             print(" " * indent + f"Comment {comment_id}")
             if child_comments:
@@ -52,22 +57,25 @@ class Transformer:
         self.posts = posts
         self.comments = comments
 
+    def run(self):
         self._createfeatures()
+        return self.posts, self.comments
 
     def _createfeatures(self):
-        ids = self.posts.id.values
+        posts_copy = self.posts.copy()
+        comments_copy = self.comments.copy()
+        ids = posts_copy.id.values
 
         # create features for posts, filling with unknown first
-        self.posts['wiener_index'] = 'unknown'
-        self.posts['depth'] = 'unknown'
-        self.posts['no_comments'] = 'unknown'
+        posts_copy["wiener_index"] = "unknown"
+        posts_copy["depth"] = "unknown"
+        posts_copy["no_comments"] = "unknown"
 
         for idd in ids:
-            tree = Tree(idd, self.comments, self.posts)
+            tree = Tree(idd, comments_copy, posts_copy)
 
             # update values
-            self.posts.loc[self.posts.id == idd,
-                           'wiener_index'] = tree.wiener_index()
-            self.posts.loc[self.posts.id == idd, 'depth'] = tree.depth()
-            self.posts.loc[self.posts.id == idd,
-                           'no_comments'] = tree.how_many_nodes()
+            posts_copy.loc[posts_copy.id == idd, "wiener_index"] = tree.wiener_index()
+            posts_copy.loc[posts_copy.id == idd, "depth"] = tree.depth()
+            posts_copy.loc[posts_copy.id == idd, "no_comments"] = tree.how_many_nodes()
+        self.posts, self.comments = posts_copy, comments_copy
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,34 +1,63 @@
 import pandas as pd
 from datetime import datetime
+import copy
 
 
 class Preprocessor:
     def __init__(self, posts, comments) -> None:
         self.posts = posts
         self.comments = comments
+
+    def run(self):
         self._drop()
         self._fillna()
         self._transform()
+        return self.posts, self.comments
 
     def _drop(self):
-        self.posts.drop(
-            columns=['ups', 'downs', 'num_comments', 'permalink'], inplace=True)
-        self.comments.drop(['score_hidden', 'archived', 'name', 'downs', 'ups', 'subreddit_id',
-                            'author_flair_css_class', 'year_month', 'distinguished'], axis=1, inplace=True)
+        posts_copy = copy.deepcopy(self.posts)
+        comments_copy = copy.deepcopy(self.comments)
+        posts_copy.drop(
+            columns=["ups", "downs", "num_comments", "permalink"], inplace=True
+        )
+        comments_copy.drop(
+            [
+                "score_hidden",
+                "archived",
+                "name",
+                "downs",
+                "ups",
+                "subreddit_id",
+                "author_flair_css_class",
+                "year_month",
+                "distinguished",
+            ],
+            axis=1,
+            inplace=True,
+        )
+        self.posts, self.comments = posts_copy, comments_copy
 
     def _fillna(self):
-        self.posts.score.fillna(0, inplace=True)
-        self.posts.selftext.fillna('[no_text]', inplace=True)
-        self.comments.body.fillna('[no_text]', inplace=True)
-        self.comments.author_flair_text.fillna('[no_text]', inplace=True)
-        self.comments.score.fillna(0, inplace=True)
+        posts_copy = copy.deepcopy(self.posts)
+        comments_copy = copy.deepcopy(self.comments)
+
+        posts_copy.score.fillna(0, inplace=True)
+        posts_copy.selftext.fillna("[no_text]", inplace=True)
+        comments_copy.body.fillna("[no_text]", inplace=True)
+        comments_copy.author_flair_text.fillna("[no_text]", inplace=True)
+        comments_copy.score.fillna(0, inplace=True)
+        self.posts, self.comments = posts_copy, comments_copy
 
     def _transform(self):
-        self.comments.created_utc = self.comments.created_utc.apply(
-            lambda x: datetime.fromtimestamp(x).isoformat())
-        self.comments.retrieved_on = self.comments.retrieved_on.apply(
-            lambda x: datetime.fromtimestamp(x).isoformat())
-        self.comments.parent_id = self.comments.parent_id.transform(
-            lambda x: x[3:])
-        self.comments.link_id = self.comments.link_id.transform(
-            lambda x: x[3:])
+        posts_copy = copy.deepcopy(self.posts)
+        comments_copy = copy.deepcopy(self.comments)
+
+        comments_copy.created_utc = comments_copy.created_utc.apply(
+            lambda x: datetime.fromtimestamp(x).isoformat()
+        )
+        comments_copy.retrieved_on = comments_copy.retrieved_on.apply(
+            lambda x: datetime.fromtimestamp(x).isoformat()
+        )
+        comments_copy.parent_id = comments_copy.parent_id.transform(lambda x: x[3:])
+        comments_copy.link_id = comments_copy.link_id.transform(lambda x: x[3:])
+        self.posts, self.comments = posts_copy, comments_copy
